7ebfa1eaf3ff4c5b07827e130daf7af7341e9669
wonjong <lej6790@gmail.com>
2025-08-13 23:08:45 +0900

    Delete mysql2

--- config/database.js
+++ config/database.js
@@ -8,7 +8,6 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST || '127.0.0.1',
         port: Number(process.env.DB_PORT || 3306),
         dialect: process.env.DB_DIALECT || 'mariadb',
-        dialectModule: require('mysql2'),
         logging: false,
         timezone: process.env.DB_TIMEZONE || '+09:00',
         pool: { max: 10, min: 0, acquire: 20000, idle: 10000 },
